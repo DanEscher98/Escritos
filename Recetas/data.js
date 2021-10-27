7df6391a0d19838d9b443add8347352e661e791b
DanEscher98 <danyiel5978@gmail.com>
2021-10-26 20:21:45 -0500

    modified:   .gitignore 	deleted:    Diary/aux.txt 	modified:   LICENSE 	modified:   Recetas/Article/Extras/harvard-cite-them-right-no-et-al.csl 	new file:   Recetas/Curriculum/CV.tex 	new file:   Recetas/Curriculum/Makefile 	new file:   Recetas/Curriculum/Preamble_CV.tex 	new file:   Recetas/Curriculum/icon.jpeg 	new file:   Recetas/Curriculum/photo.jpeg 	modified:   Recetas/TexBook/_markdown_main/2700052c3778f012282cd8d50e70e890.md.tex 	modified:   Recetas/TexBook/_markdown_main/2735628b6d6c09b7b7ffbb54c9ec0bd2.md.tex 	modified:   Recetas/TexBook/_markdown_main/5d0244496c4f2c796e887b6ed1b1a2af.md.tex 	deleted:    Recetas/aux.txt 	new file:   Recetas/data.js 	modified:   Recetas/mdLetter/letter.md 	modified:   ShortStories/Makefile 	modified:   ShortStories/TODO.md 	modified:   ShortStories/index.html

---
+++ Recetas/data.js
@@ -0,0 +1,99 @@
+const HEADER = "Title";
+
+const NAVBAR_DATA = [
+	{ id: 1, url: "/",				label: "Home" },
+	{ id: 2, url: "#",		label: "" },
+];
+
+const BANNER_DATA = {
+	HEADING: "",
+	DESCRIPTION: ""
+};
+
+const SERVICE_DATA = {
+	HEADING: "Our Services",
+	ALL_SERVICES: "All Services",
+	SERVICE_LIST: [
+		{
+			LABEL: "",
+			DESCRIPTION: "",
+			URL: "images/photo1.png"
+		},
+		{
+			LABEL: "",
+			DESCRIPTION: "",
+			URL: "images/photo2.png"
+		},
+		{
+			LABEL: "",
+			DESCRIPTION: "",
+			URL: "images/photo3.png"
+		}
+	]
+};
+
+const ABOUT_DATA = {
+	HEADING: "",
+	TITLE: "",
+	IMAGE_URL: "",
+	WHY_CHOOSE_US_LIST: [
+		"",
+		"",
+		"",
+		""
+	]
+};
+
+const TESTIMONIAL_DATA = {
+	HEADING: "",
+	TESTIMONIAL_LIST: [
+		{
+			DESCRIPTION: "",
+			IMAGE_URL: "",
+			NAME: "",
+			DESIGNATION: ""
+		},
+		{
+			DESCRIPTION: "",
+			IMAGE_URl: "",
+			NAME: "",
+			DESIGNATION: ""
+		}
+	]
+};
+
+const SOCIAL_DATA = {
+	HEADING: "",
+	IMAGES_LIST: [
+		"",
+		"",
+		"",
+		"",
+		""
+	]
+};
+
+const FOOTER_DATA = {
+	DESCRIPTION: "",
+	CONTACT_DETAILS: {
+		HEADING: "Contact us",
+		ADDRESS: "",
+		MOBILE: "+1 61234567890",
+		EMAIL: "email@example.com"
+	},
+	SUBSCRIBE_NEWSLETTER: "Subscribe newsletter",
+	SUBSCRIBE: "Subscribe"
+};
+
+const MOCK_DATA = {
+	HEADER,
+	NAVBAR_DATA,
+	BANNER_DATA,
+	SERVICE_DATA,
+	ABOUT_DATA,
+	TESTIMONIAL_DATA,
+	SOCIAL_DATA,
+	FOOTER_DATA
+};
+
+export default MOCK_DATA;
