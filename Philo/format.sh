6d1cc0e09b950537b0668c637aaa5bd272d8a2a8
Danyiel Colin <danyiel5978@gmail.com>
2022-04-28 00:33:22 -0500

    modified:   format.sh

--- Philo/format.sh
+++ Philo/format.sh
@@ -9,13 +9,14 @@
 
 
 for file_name in $(ls Files); do
-	num = $(echo $file_name | sed -n 's/\([0-9]*\)_.*$/\1/p')
-	name = $(echo $file_name \
+	num=$(echo $file_name | sed -n 's/\([0-9]*\)_.*$/\1/p')
+	name=$(echo $file_name \
 		| sed -n 's/[0-9]_\(\S*\).md$/\1/p')
 	if [ ! grep -qF "$num $name" titles.txt 2>&1 ]; then
 		echo "$num $name" >> titles.txt
 	fi
 	mv Files/"$file_name" Files/$num.md
+	echo "$num $name"
 done
 
 
