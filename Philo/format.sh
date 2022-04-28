d0c3abc59af9217b8384dedc8382b8f9db39e966
Danyiel Colin <danyiel5978@gmail.com>
2022-04-28 01:21:20 -0500

    modified:   format.sh modified:   titles.txt

--- Philo/format.sh
+++ Philo/format.sh
@@ -9,28 +9,28 @@
 
 
 for file_name in $(ls Files); do
-        num=$(echo $file_name | sed -n 's/\([0-9]*\)_.*$/\1/p')
-        name=$(echo $file_name \
-                | sed -n 's/[0-9]_\(\S*\)\.md$/\1/p')
+    num=$(echo $file_name | sed -n 's/\([0-9]*\)_.*$/\1/p')
+    name=$(echo $file_name \
+        | sed -n 's/[0-9]_\(\S*\)\.md$/\1/p')
 
-        grep -qF "$num $name" titles.txt 2>&1
-        if [ $? -ne 0 ]; then
-                echo "$num $name" >> titles.txt
-        fi
-        if [ -n "$num" ]; then
-                mv Files/"$file_name" Files/$num.md
-                echo "rename to: $num.md"
-        fi
+    grep -qF "$num $name" titles.txt 2>&1
+    if [ $? -ne 0 ]; then
+        echo "$num $name" >> titles.txt
+    fi
+    if [ -n "$num" ]; then
+        mv Files/"$file_name" Files/$num.md
+        echo "rename to: $num.md"
+    fi
 done
 
 echo $(ls Files)
 
 for file_name in $(ls Files); do
-        num=$(echo $file_name | sed -n 's/\([0-9]*\)\.md$/\1/p')
-        name=$(grep -w "$num" titles.txt | awk '{ print $2 }')
+    num=$(echo $file_name | sed -n 's/\([0-9]*\)\.md$/\1/p')
+    name=$(grep -w "$num" titles.txt | awk '{ print $2 }')
 
-        if [ -n "$num" && -n "$name"]; then
-                                echo "rename to: $num_$name.md"
-                mv Files/"$num".md Files/"$num_$name".md
-        fi
+    if [ -n "$num" ] && [ -n "$name" ]; then
+        echo "rename to: $num\_$name.md"
+        mv Files/"$num".md Files/"$num"_"$name".md
+    fi
 done
